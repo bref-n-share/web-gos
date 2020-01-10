eac9af5fdb38c0a3c120fd1152646a5ca2f959c8
Stéphane Conq <conqstephane@gmail.com>
2020-01-10 15:28:03 +0100

    created form, need to link to back

--- src/app/services/demands.service.ts
+++ src/app/services/demands.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,11 @@ export class DemandsService {
   demandsBS = new BehaviorSubject([]);
   allDemands = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createRequest(payload) {
+    return this.http.post(`${environment.apiUrl}/post/request`, payload);
+  }
 }
