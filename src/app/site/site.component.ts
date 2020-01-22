9affed0483d7b72e5d585818d3742b9941d34aab
Plaza Bastien <plaza.bastien@neuf.fr>
2020-01-22 16:52:17 +0100

    descrition site

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -3,6 +3,9 @@ import { AgmCoreModule } from '@agm/core';
 import {MatDialog} from '@angular/material';
 import {SiteDetailsComponent} from './site-details/site-details.component';
 import {MapsService} from '../services/maps.service';
+import {StructureService} from '../services/structure.service';
+import {Site} from '../models/Site';
+import {User} from '../models/User';
 
 @Component({
   selector: 'app-orga',
@@ -13,33 +16,46 @@ export class SiteComponent implements OnInit {
 
   constructor(
     private dialog: MatDialog,
-    private mapsService: MapsService
+    private mapsService: MapsService,
+    private structureService: StructureService
   ) { }
 
-  orga = {
-    lat: 45.751785,
-    lng: 4.849233,
-    name: 'Emmaüs Créqui Lyon',
-    tel: '0987654321',
-    address: '283 Rue de Créqui, 69007 Lyon'
-  };
+  site: Site;
+  user: User;
+  latitude = 0;
+  longitude = 0;
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+    this.structureService.getSite(this.user.structure.id).subscribe((ret: Site) => {
+      this.site = ret;
+      this.latitude = parseFloat(ret.latitude);
+      this.longitude = parseFloat(ret.longitude);
+    });
   }
 
   onMarkerClick() {
     const ref = this.dialog.open(SiteDetailsComponent, {
-      data: {...this.orga}
+      data: {...this.site}
     });
 
-    ref.afterClosed().subscribe(orga => {
-      if (orga) {
-        if (JSON.stringify(orga) !== JSON.stringify(this.orga)) {
-          this.mapsService.findLocation(orga.address).then((result) => {
-            console.log('result', result);
-            this.orga.lng = result['lng'];
-            this.orga.lat = result['lat'];
-            this.orga.address = result['formatted_address'];
+    ref.afterClosed().subscribe((site: Site) => {
+      if (site) {
+        if (JSON.stringify(site) !== JSON.stringify(this.site)) {
+          this.mapsService.findLocation(site.address).then((result) => {
+            this.site.longitude = result['lng'];
+            this.site.latitude = result['lat'];
+            this.site.address = result['formatted_address'];
+            this.latitude = result['lat'];
+            this.longitude = result['lng'];
+            this.structureService.updateSite(this.site.id,
+              {longitude: this.site.longitude.toString(), latitude: this.site.latitude.toString(), address: this.site.address,
+                      phone: site.phone.toString(), description: site.description.toString()})
+              .subscribe((ret: Site) => {
+                this.site = site;
+              }, error => {
+                console.log(error);
+              });
           });
         }
       }
